d88120ad1b56ae055550b697f3e0a70c739d266d
Omindu Hirushka <102536488+OminduHirushka@users.noreply.github.com>
2025-04-01 17:43:33 +0530

    delete post

--- index.js
+++ index.js
@@ -70,10 +70,33 @@ app.put("/api/v1/update-posts/:id", async (req, res) => {
   const { blog_title, blog_content } = req.body;
 
   try {
-    const update_query = "UPDATE blogs SET blog_title=$1, blog_content=$2 WHERE blog_id=$3  RETURNING *";
+    const update_query =
+      "UPDATE blogs SET blog_title=$1, blog_content=$2 WHERE blog_id=$3  RETURNING *";
 
-    const result = await connection.query(update_query, [blog_title, blog_content, id]);
+    const result = await connection.query(update_query, [
+      blog_title,
+      blog_content,
+      id,
+    ]);
     res.status(200).json(result.rows[0]);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.delete("/api/v1/delete-posts/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const delete_query = "DELETE FROM blogs WHERE blog_id=$1";
+
+    const result = await connection.query(delete_query, [id]);
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    res.status(200).json({ message: "User deleted successfully" });
 
   } catch (error) {
     console.log(error);
