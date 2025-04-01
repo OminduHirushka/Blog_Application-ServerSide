e8bf3f57d055dc6785f1920e5f296f29befc9d7d
Omindu Hirushka <102536488+OminduHirushka@users.noreply.github.com>
2025-04-01 16:08:56 +0530

    basic Express server configuration

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const { Client } = require("pg");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 const connection = new Client({
   host: "localhost",
@@ -8,4 +16,8 @@ const connection = new Client({
   database: "blogapp",
 });
 
-connection.connect().then(() => console.log("Connected Succefully!"));
+connection.connect().then(() => console.log("Connected Successfully!"));
+
+app.listen(3000, (req, res) => {
+  console.log("Server is running on port 3000");
+});
