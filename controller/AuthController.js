adf04a9f9315b7d9eff843c008abf74b137a14ed
Omindu Hirushka <102536488+OminduHirushka@users.noreply.github.com>
2025-04-03 06:48:43 +0530

    user login

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -42,3 +42,51 @@ exports.signup = async (req, res) => {
   }
 };
 
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const findUserQuery = "SELECT * FROM users WHERE email = $1";
+    const result = await connection.query(findUserQuery, [email]);
+
+    if (result.rows.length === 0) {
+      return res.status(403).json({ message: "User not found" });
+    }
+
+    const user = result.rows[0];
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(403).json({ message: "Incorrect password" });
+    }
+
+    const payload = {
+      id: user.id,
+      email: user.email,
+      firstName: user.first_name,
+      lastName: user.last_name,
+      type: user.type,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_KEY, {
+      expiresIn: "48h",
+    });
+
+    const userResponse = {
+      id: user.id,
+      firstName: user.first_name,
+      lastName: user.last_name,
+      email: user.email,
+      type: user.type,
+    };
+
+    res.json({
+      message: "Login successful",
+      user: userResponse,
+      token: token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const authController = require("../controller/AuthController");
 
 router.post("/signup", authController.signup);
+router.post("/login", authController.login);
 
 module.exports = router;
